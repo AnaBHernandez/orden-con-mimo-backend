2da9f431e1765f9808b84301dc7c40bf0d70fc14
AnaBHernandez <[anahernandezfit@gmail.com]>
2025-03-31 17:35:28 +0200

    Configuración inicial del proyecto Spring Boot

---
+++ src/main/java/com/ordenconmimo/OrdenconmimoApplication.java
@@ -0,0 +1,13 @@
+package com.ordenconmimo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdenconmimoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrdenconmimoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ordenconmimo/OrdenconmimoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ordenconmimo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdenconmimoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
